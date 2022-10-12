6fff19218cb8c101d0c4cbb52b49e9696d40a4a0
Nicolas Kingreen <NicolasKingreen@gmail.com>
2022-10-12 14:59:20 +0300

    * outer padding * some refactoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 def get_gaussian_kern(sigma=1.5, size=3):
     """
-    size gotta be odd (3, 5, 7, ...)
+    size gotta be odd (e. g. 3, 5, 7, ...)
     """
     indices = np.dstack(np.indices((size, size)))
     indices -= (size//2, size//2)  # offsetting coordinates
@@ -19,27 +19,28 @@ def get_gaussian_kern(sigma=1.5, size=3):
     return gaussian_matrix
 
 
-def gaussian_blur(pixels, sigma=1.5, size=3):
-    pixels = np.asarray(pixels)
+def gaussian_blur(image: Image, sigma=1.5, size=3):
+    a = np.array(image)
     kern = get_gaussian_kern(sigma, size)
 
-    height, width = pixels.shape[:2]
-    for x in range(1, width-1):
-        for y in range(1, height-1):
-            center = y+1, x+1
+    a = np.pad(a, ((1, 1), (1, 1), (0, 0)), mode="edge")
 
-            area = pixels[y-1:y+2, x-1:x+2]
+    height, width = a.shape[:2]
+    for y in range(1, height - 1):
+        for x in range(1, width-1):
+            center = y+size//2, x+size//2
+
+            area = a[y-size//2:y+size//2+1, x-size//2:x+size//2+1]
 
             red = np.ceil(np.sum(area[..., 0] * kern)).astype("uint8")
             green = np.ceil(np.sum(area[..., 1] * kern)).astype("uint8")
             blue = np.ceil(np.sum(area[..., 2] * kern)).astype("uint8")
 
-            pixels[center] = np.dstack((red, green, blue))
-    return pixels
+            a[center] = np.dstack((red, green, blue))
+    return a[1:-1, 1:-1]
 
 
 image = Image.open("image.jpg")
-pixels = np.array(image)
-pixels = gaussian_blur(pixels)
+pixels = gaussian_blur(image, sigma=10)
 plt.imshow(pixels)
 plt.show()
